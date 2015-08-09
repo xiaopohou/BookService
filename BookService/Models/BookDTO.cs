4b3ef7d37fceec4bd926e3803efa7744b67a8afb
ericjohnston <ericjohnston+git@gmail.com>
2015-08-08 20:10:24 -0400

    Initial commit

---
+++ BookService/Models/BookDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookService.Models
+{
+    public class BookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+    }
+
+    public class BookDetailDTO : BookDTO
+    {
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+    }
+}
